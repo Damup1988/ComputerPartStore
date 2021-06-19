b3e626c171f62cf08ebac34c1295f2bf225f929e
Damup1988 <damup1988@gmail.com>
2021-06-19 15:44:41 +0300

    public interface IItemClient - functions have been added

--- ComputerPartStore.BusinessLogic/IItemClient.cs
+++ ComputerPartStore.BusinessLogic/IItemClient.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using ComputerPartStore.Domain;
 
@@ -7,5 +8,10 @@ namespace ComputerPartStore.BusinessLogic
     public interface IItemClient
     {
         IEnumerable<Item> GetAllItems();
+        Item GetItemByName(string itemName);
+        Item GetItemById(Guid Id);
+        void AddNewItem(Item newItem);
+        void UpdateItem(Item existingItem);
+        void DeleteItem(Item itemToRemove);
     }
 }
--- ComputerPartStore.DB/IItemRepository.cs
+++ ComputerPartStore.DB/IItemRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using ComputerPartStore.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace ComputerPartStore.DB
 {
