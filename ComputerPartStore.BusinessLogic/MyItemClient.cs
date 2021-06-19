d3b8e8f8c85b8cdea2b5044b8d1896d436466fe4
Damup1988 <damup1988@gmail.com>
2021-06-19 20:50:39 +0300

    MuItemClient.cs has been added

---
+++ ComputerPartStore.BusinessLogic/MyItemClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using ComputerPartStore.Domain;
+
+namespace ComputerPartStore.BusinessLogic
+{
+    public class MyItemClient : IItemClient
+    {
+        public IEnumerable<Item> GetAllItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetItemByName(string itemName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetItemById(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddNewItem(Item newItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(Item existingItem, string itemPropertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
